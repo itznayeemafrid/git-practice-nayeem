cec379fb45018840ffb60e7a54ef6aecf9a15da3
itznayeemafrid <nayeemislam007404@.com>
2026-09-02 14:48:04 +0600

    Basic error-handling

--- src/main.py
+++ src/main.py
@@ -5,4 +5,8 @@ print("Name: Nayeem Islam")
 print("Today's Date:", datetime.date.today())
 print("5 + 3 =", add(5, 3))
 print("10 - 4 =", subtract(10, 4))
-print("10 / 0 =", divide(10, 0))
+
+try:
+    print("10 / 0 =", divide(10, 0))
+except ZeroDivisionError as e:
+    print("Error:", e)
