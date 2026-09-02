e55ce3f6b74886967f2888900b690bd7aecf3c62
itznayeemafrid <nayeemislam007404@.com>
2026-09-02 14:32:20 +0600

    Just before task 5

--- src/main.py
+++ src/main.py
@@ -1,13 +1,4 @@
-# Main execution script for calculator functions
-
 import datetime
-from utils import add, subtract
 
 print("Name: Nayeem Islam")
 print("Today's Date:", datetime.date.today())
-print("5 + 3 =", add(5, 3))
-print("10 - 4 =", subtract(10, 4))
-try:
-    print("10 / 0 =", divide(10, 0))
-except ValueError as e:
-    print("Error:", e)
